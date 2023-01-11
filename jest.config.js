9cb8207511dca134b7c0063d2f69ef44ec519570
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-11 16:08:35 -0300

    chore: add varias configuracoes jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  // collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'

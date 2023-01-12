584b21de4b622d832eddbe11e9f85feb5fda776b
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-12 11:54:23 -0300

    chore: configuracao do jest com o mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   // collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'

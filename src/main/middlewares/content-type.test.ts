8418b629911b80863c5fe1e994749bc028d96e4b
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-16 11:58:23 -0300

    test: garante que a requisicao retorna um json por padrao o content-type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  test('Deve retornar por default content-type como JSON', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Deve retornar o content-type como xml quando forçado', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})

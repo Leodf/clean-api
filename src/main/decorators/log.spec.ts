f66ec502fda1d2e655c22d41cad8ac1881c7b6d1
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-16 15:53:01 -0300

    refactor: movendo o sut de criacao para acima do describe

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,34 +1,35 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
-describe('LogController Decorator', () => {
-  const makeController = (): Controller => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'Leonardo'
-          }
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Leonardo'
         }
-        return await new Promise(resolve => { resolve(httpResponse) })
       }
+      return await new Promise(resolve => { resolve(httpResponse) })
     }
-    return new ControllerStub()
   }
+  return new ControllerStub()
+}
 
-  interface SutTypes {
-    sut: LogControllerDecorator
-    controllerStub: Controller
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
   }
-  const makeSut = (): SutTypes => {
-    const controllerStub = makeController()
-    const sut = new LogControllerDecorator(controllerStub)
-    return {
-      sut,
-      controllerStub
-    }
-  }
+}
+
+describe('LogController Decorator', () => {
   test('Deve chamar o controller handle', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')

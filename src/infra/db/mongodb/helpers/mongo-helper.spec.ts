0fe546d777a490cd57481ca92880f0567a5f9d65
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-17 10:34:23 -0300

    refactor: skipando o teste para nao abrir conexoes desnecessarias

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,6 +1,6 @@
 import { MongoHelper as sut } from './mongo-helper'
 
-describe('Mongo Helper', () => {
+describe.skip('Mongo Helper', () => {
   beforeAll(async () => {
     await sut.connect(process.env.MONGO_URL)
   })

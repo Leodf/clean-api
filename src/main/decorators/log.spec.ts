b27a352a01d5b1e86d1a322846844e58cb63edb1
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-17 10:01:18 -0300

    test: garante LogMongoRepository cria um log de erro no sucesso

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -20,7 +20,7 @@ const makeController = (): Controller => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log (stackError: string): Promise<void> {
+    async logError (stackError: string): Promise<void> {
       await new Promise<void>(resolve => { resolve() })
     }
   }
@@ -75,7 +75,7 @@ describe('LogController Decorator', () => {
     const fakeError = new Error()
     fakeError.stack = 'any_stack'
     const error = serverError(fakeError)
-    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
     jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => { resolve(error) }))
     await sut.handle(makeFakeRequest())
     expect(logSpy).toHaveBeenCalledWith('any_stack')

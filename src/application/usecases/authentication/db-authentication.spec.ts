022c4b89225dd2eb2f5b9b3a790b2da88d2a2e34
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-18 15:43:24 -0300

    test: garante DbAuthentication lanca erro se LoadAccountByEmailRepository lancar erro

--- src/application/usecases/authentication/db-authentication.spec.ts
+++ src/application/usecases/authentication/db-authentication.spec.ts
@@ -45,4 +45,10 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
+  test('Deve lançar erro se LoadAccountByEmailRepository lançar', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })

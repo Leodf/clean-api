b65b7637f0d7f6cfa335b29247318439dc5a4fdb
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-18 16:07:46 -0300

    test: garante DbAuthentication lanca erro se HashCompare lancar erro

--- src/application/usecases/authentication/db-authentication.spec.ts
+++ src/application/usecases/authentication/db-authentication.spec.ts
@@ -69,10 +69,16 @@ describe('DbAuthentication UseCase', () => {
     const accessToken = await sut.auth(makeFakeAuthentication())
     expect(accessToken).toBe(null)
   })
-  test('Deve chamar HashComparer com o password correto', async () => {
+  test('Deve chamar HashComparer com os valores corretos', async () => {
     const { sut, hashComparerStub } = makeSut()
     const compareSpy = jest.spyOn(hashComparerStub, 'compare')
     await sut.auth(makeFakeAuthentication())
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+  })
+  test('Deve lançar erro se HashComparer lançar', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
   })
 })

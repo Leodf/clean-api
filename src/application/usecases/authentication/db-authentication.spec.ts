fd2d619db0dbc4a5383a2a7317a497e4c6c80d3c
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-18 16:30:37 -0300

    test: garante DbAuthentication lanca erro se TokenGenerator lancar erro

--- src/application/usecases/authentication/db-authentication.spec.ts
+++ src/application/usecases/authentication/db-authentication.spec.ts
@@ -106,4 +106,10 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(generateSpy).toHaveBeenCalledWith('any_id')
   })
+  test('Deve lançar erro se TokenGenerator lançar', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })

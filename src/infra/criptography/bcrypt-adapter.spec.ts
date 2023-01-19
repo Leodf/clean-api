48ef1db526c46759e303ab8d94153574870e2c83
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-19 09:58:29 -0300

    feat: garante BCryptAdapter retorna false quando compare falhar

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -48,4 +48,10 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
+  test('Deve retornar false quando compare falhar', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => await Promise.resolve(false))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
 })

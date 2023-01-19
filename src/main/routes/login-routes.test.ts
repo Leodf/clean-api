9dee856f5dff9809acdd81f0023e1bf02a254441
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-19 15:27:20 -0300

    refactor: renomeando o signup routes para login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,15 +15,18 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Deve retornar uma account no sucesso', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Leonardo',
-        email: 'faver_i@hotmail.com',
-        password: '123456',
-        passwordConfirmation: '123456'
-      })
-      .expect(200)
+
+  describe('POST /signup', () => {
+    test('Deve retornar 200 no signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Leonardo',
+          email: 'faver_i@hotmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
   })
 })

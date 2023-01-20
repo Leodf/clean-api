d2470ffcc60ebab06fd6bb159ff254467d5af008
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-20 12:05:51 -0300

    chore: configurando os scripts para docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-api',
   port: process.env.PORT || 5050,
   salt: parseInt(process.env.SALT) || 12,
   jwtSecret: process.env.JWT_SECRET || 'algo_secreto'

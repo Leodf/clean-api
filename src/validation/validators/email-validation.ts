a07145ff2b51724787efab5da8c92e0583013e9c
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-25 13:20:14 -0300

    refactor: movendo o validation para sua propria camada

--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,7 +1,7 @@
 
-import { InvalidParamError } from '../../errors'
-import { EmailValidator } from '../../protocols/email-validator'
-import { Validation } from '../../protocols/validation'
+import { InvalidParamError } from '../../presentation/errors'
+import { EmailValidator } from '../protocols/email-validator'
+import { Validation } from '../../presentation/protocols/validation'
 
 export class EmailValidation implements Validation {
   constructor (

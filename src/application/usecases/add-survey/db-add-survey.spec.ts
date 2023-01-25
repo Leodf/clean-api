891ab17ff2e0769b658b6b23300b4cc2700ed8c8
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-25 18:44:41 -0300

    test: garante DbAddSurvey lanca erro se AddSurveyRepository lanca erro

--- src/application/usecases/add-survey/db-add-survey.spec.ts
+++ src/application/usecases/add-survey/db-add-survey.spec.ts
@@ -40,4 +40,11 @@ describe('DbAddSurvey Usecase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
+  test('Deve lançar erro se AddSurveyRepository lançar erro', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const surveyData = makeFakeSurveyData()
+    const promise = sut.add(surveyData)
+    await expect(promise).rejects.toThrow()
+  })
 })

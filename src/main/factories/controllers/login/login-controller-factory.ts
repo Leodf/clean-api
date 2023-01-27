324267f566af25ace50c50c643d8c68b748f83c0
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-27 10:18:21 -0300

    refactor: organizando em pastas na camada main

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../../presentation/protocols'
 import { LoginController } from '../../../../presentation/controllers/login/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
-import { makeDbAthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeDbAthentication } from '../../usecases/account/authentication/db-authentication-factory'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeLoginController = (): Controller => {

71334e244792f4ea1ce15d0dcbbfe5a733b3743e
Leonardo de Faveri <faver_i@hotmail.com>
2023-01-31 16:54:18 -0300

    feat: garante DbLoadSurveyById chama o LoadSurveyByIdRepository com o valor correto

---
+++ src/application/usecases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,12 @@
+import { LoadSurveyByIdRepository } from '@/application/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor (private readonly loadByIdRepository: LoadSurveyByIdRepository) {}
+
+  async loadById (id: string): Promise<SurveyModel> {
+    await this.loadByIdRepository.loadById(id)
+    return await new Promise(resolve => { resolve(null!) })
+  }
+}

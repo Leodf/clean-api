c5679ae1cef1ab17b68dbd16066c8790074dae40
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-01 10:43:19 -0300

    test: garante DbSaveSurveyResult lanca erro se SaveSurveyResultRepository lancar erro

--- src/application/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/application/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -48,4 +48,11 @@ describe('DbSaveSurveyResult Usecase', () => {
     await sut.save(surveyResultData)
     expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
   })
+  test('Deve lançar erro se SaveSurveyResultRepository lançar erro', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(saveSurveyResultRepositoryStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const surveyResultData = makeFakeSurveyResultData()
+    const promise = sut.save(surveyResultData)
+    await expect(promise).rejects.toThrow()
+  })
 })

8c96d018dfb97650f269fd95a0fa6975258a9593
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-01 11:30:51 -0300

    refactor: tirando await do getCollection do log de error

--- src/infra/db/mongodb/log/log-mongo-repository.ts
+++ src/infra/db/mongodb/log/log-mongo-repository.ts
@@ -3,7 +3,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class LogMongoRepository implements LogErrorRepository {
   async logError (stackError: string): Promise<void> {
-    const errorCollection = await MongoHelper.getCollection('errors')
+    const errorCollection = MongoHelper.getCollection('errors')
     await errorCollection.insertOne({
       stackError,
       date: new Date()

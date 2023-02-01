2df058927744bf0b347a5f41e4278a02eaeb0232
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-01 11:08:58 -0300

    feat: garante SurveyMongoRepository encontra uma survey by id no sucesso

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,12 @@
-import { AddSurveyRepository } from '@/application/protocols/db/survey/add-survey-repository'
-import { LoadSurveysRepository } from '@/application/protocols/db/survey/load-surveys-repository'
 import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
+import { AddSurveyRepository } from '@/application/protocols/db/survey/add-survey-repository'
+import { LoadSurveysRepository } from '@/application/protocols/db/survey/load-surveys-repository'
+import { LoadSurveyByIdRepository } from '@/application/protocols/db/survey/load-survey-by-id-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
@@ -13,7 +15,14 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
   async loadAll (): Promise<SurveyModel[]> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     const surveysData = await surveyCollection.find().toArray()
-    const surveys = MongoHelper.mapCollection(surveysData)
+    const surveys = MongoHelper.mapCollection(surveysData) as SurveyModel[]
     return surveys
+  }
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = MongoHelper.getCollection('surveys')
+    const surveyData = await surveyCollection.findOne({ _id: new ObjectId(id) })
+    const survey = MongoHelper.map(surveyData) as SurveyModel
+    return survey
   }
 }

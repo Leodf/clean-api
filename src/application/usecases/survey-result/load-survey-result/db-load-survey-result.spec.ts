2369f6ba7b3470a18ce8b658177d9e52cdcdd635
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-07 16:56:04 -0300

    test: garante DbLoadSurveyResult chama LoadSurveyResultRepository com os valores corretos

---
+++ src/application/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,19 @@
+import { LoadSurveyResultRepository } from '@/application/protocols/db/survey-result/load-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { mockSurveyResultModel } from '@/domain/tests'
+import { DbLoadSurveyResult } from './db-load-survey-result'
+
+describe('DbLoadSurveyResult UseCase', () => {
+  test('Deve chamar LoadSurveyResultRepository com os valores corretos', async () => {
+    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+        return await Promise.resolve(mockSurveyResultModel())
+      }
+    }
+    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_survey_id')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+})

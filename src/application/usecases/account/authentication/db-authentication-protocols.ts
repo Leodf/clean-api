257b29e769fdb748ae5ff27c78a4ab4f20097022
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-08 13:21:10 -0300

    feat: add name para a resposta do authentication

--- src/application/usecases/account/authentication/db-authentication-protocols.ts
+++ src/application/usecases/account/authentication/db-authentication-protocols.ts
@@ -1,5 +1,6 @@
 export * from '@/domain/models/account'
 export * from '@/domain/usecases/account/authentication'
+export * from '@/domain/models/authentication'
 export * from '@/application/protocols/db/account/load-account-by-email-repository'
 export * from '@/application/protocols/db/account/update-access-token-repository'
 export * from '@/application/protocols/cryptography/hash-compare'

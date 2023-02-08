28f7fc0c9e207953bcb15d41bc409eef9f641c72
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-08 09:32:37 -0300

    feat: garante DbLoadSurveyResult chama LoadSurveyByIdRepository com os valores corretos

--- src/application/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
+++ src/application/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -1,3 +1,4 @@
-export * from '@/application/protocols/db/survey-result/load-survey-result-repository'
 export * from '@/domain/models/survey-result'
 export * from '@/domain/usecases/survey-result/load-survey-result'
+export * from '@/application/protocols/db/survey-result/load-survey-result-repository'
+export * from '@/application/protocols/db/survey/load-survey-by-id-repository'

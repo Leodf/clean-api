2153ea7bff2ad6ca9f2a5310025690d8bf341a8b
Leonardo de Faveri <faver_i@hotmail.com>
2023-02-27 16:27:40 -0300

    test: garante Login Query retorna uma account com credenciais validas

---
+++ tests/main/graphql/login-routes.test.ts
@@ -0,0 +1,47 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Express } from 'express'
+import { setupApp } from '@/main/config/app'
+import { hash } from 'bcrypt'
+import request from 'supertest'
+
+let accountCollection: Collection
+let app: Express
+
+describe('Login GraphQl', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  describe('Login Query', () => {
+    const query = `query {
+      login (email: "faver_i@hotmail.com", password: "123456") {
+        accessToken
+        name
+      }
+    }`
+    test('Deve retornar um account com credenciais validas', async () => {
+      const password = await hash('123456', 12)
+      await accountCollection.insertOne({
+        name: 'Leonardo',
+        email: 'faver_i@hotmail.com',
+        password
+      })
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.login.accessToken).toBeTruthy()
+      expect(res.body.data.login.name).toBe('Leonardo')
+    })
+  })
+})

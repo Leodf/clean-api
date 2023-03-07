e376c99492efc449346936e81f0080973462d709
Leonardo de Faveri <faver_i@hotmail.com>
2023-03-07 11:47:26 -0300

    test: garante SurveyResult Query retorna AccessDeniedError se o token de acesso nao for fornecido

--- tests/main/graphql/survey-result-routes.test.ts
+++ tests/main/graphql/survey-result-routes.test.ts
@@ -94,5 +94,39 @@ describe('Survey Result GraphQl', () => {
         isCurrentAccountAnswer: false
       }])
     })
+    test('Deve retornar AccessDeniedError se o token nao for fornecido', async () => {
+      const now = new Date()
+      const surveyResult = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2'
+          }
+        ],
+        date: now
+      })
+      const query = `query {
+        loadSurveyResult (surveyId: "${surveyResult.insertedId.toHexString()}") {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }`
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(403)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Access denied')
+    })
   })
 })
